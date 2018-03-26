a2c249dddea37019b3bebceb2574d8f9262dd48a
unknown <guoyuquan@QL-01-0591.ql.com>
2018-03-26 15:19:01 +0800

    re config

--- src/app/admin/manage-center.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template:  `
-    <p>Manage your center here</p>
-  `
-})
-export class ManageCenter { }
--- src/app/center/center-home.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: `
-    <h3>Welcome to the Center Home</h3>
-  `
-})
-export class CenterHome {}
